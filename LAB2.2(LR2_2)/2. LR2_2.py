d2854e5f7394ebd184522ba2b0c415de4c1500d5
Louise Cacananta <123799928+Ruwweez@users.noreply.github.com>
2024-04-28 01:16:36 +0800

    Create 2. LR2_2.py

---
+++ LAB2.2(LR2_2)/2. LR2_2.py
@@ -0,0 +1,19 @@
+fileName = input('Enter file name: ')
+lines = []
+try:
+    with open(fileName, 'r') as f:
+        for line in f:
+            lines.append(line.strip())
+        print("The file has", len(lines), "lines.")
+        while True:
+            lineNumber = int(input("Enter a line number [0 to quit]: "))
+            if lineNumber == 0:
+                break
+            elif lineNumber < 0 or lineNumber > len(lines):
+                print("ERROR: line number must be between 1 and", len(lines))
+            else:
+                print("Line", lineNumber, ": ", lines[lineNumber - 1])
+except FileNotFoundError:
+    print("File not found. Please make sure the file exists and try again.")
+except ValueError:
+    print("Invalid input. Please enter a valid line number.")
